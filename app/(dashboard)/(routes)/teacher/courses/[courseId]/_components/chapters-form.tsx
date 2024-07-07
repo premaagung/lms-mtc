69ab8da4057995f4ca769aabe527554c236b7fd9
Agung Prema <agungprema71@gmail.com>
2024-07-07 16:02:56 +0800

    modified chapter-form.tsx

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
@@ -23,6 +23,8 @@ import {
 import { title } from "process";
 import { Input } from "@/components/ui/input";
 import { text } from "stream/consumers";
+import { ChaptersList } from "./chapters-list";
+import { list } from "postcss";
 
 interface ChapterFormsProps {
     initialData: Course & { chapters: Chapter[] };
@@ -66,8 +68,33 @@ export const ChapterForm= ({
         }
     } 
 
+    const onReorder = async (updateData: { id: string;  position: number }[]) => {
+        try {
+            setIsUpdating(true);
+
+            await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+                list: updateData
+            });
+             toast.success("Chapter reordered");
+             router.refresh();
+        } catch {
+            toast.error("Something went wrong");
+        } finally {
+            setIsUpdating(false);
+        }
+    }
+
+    const onEdit = (id: string) => {
+        router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+    }
+
     return (
-        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+        <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+            {isUpdating && (
+                <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center">
+                    <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+                </div>
+            )}
             <div className="font-medium flex items-center justify-between">
                 Course chapters
                 <Button onClick={toggleCreating} variant="ghost">
@@ -118,7 +145,11 @@ export const ChapterForm= ({
                     !initialData.chapters.length && "text-slate-500 italic"
                 )}>
                     {!initialData.chapters.length && "No chapters"}
-                    {/* TODO: Add a list of chapters */}
+                    <ChaptersList 
+                        onEdit={onEdit}
+                        onReorder={onReorder}
+                        items={initialData.chapters || []}
+                    />
                 </div>
             )}
             {!isCreating && (
