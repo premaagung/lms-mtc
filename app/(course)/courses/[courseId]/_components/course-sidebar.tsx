8cd9d0725b3a06d38dfa305e54ef72a00e7d204e
Agung Prema <agungprema71@gmail.com>
2024-07-18 02:06:10 +0800

    implement chapter progress functionality

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 
 import { db } from "@/lib/db";
+import { CourseProgress } from "@/components/course-progress";
 
 import { Chapter, Course, UserProgress } from "@prisma/client"
 import { redirect } from "next/navigation";
@@ -41,7 +42,14 @@ export const CourseSidebar = async ({
                 <h1 className="font-semibold">
                     {course.title}
                 </h1>
-                {/* {Check purchase and add progress} */}
+                {purchase && (
+                    <div className="mt-10">
+                        <CourseProgress 
+                            variant="success"
+                            value={progressCount}
+                        />
+                    </div>
+                )}
             </div>
             <div className="flex flex-col w-full">
                 {course.chapters.map((chapter) => (
